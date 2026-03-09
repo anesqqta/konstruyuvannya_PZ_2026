40b53f1753c8544744927a86a87f0fecddaad105
anesqqta <anastasiya.semeniuk@kpk.lp-com.ua>
2026-03-09 10:03:38 +0200

    Програма до рефакторингу

---
+++ PR4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        string role = "Student";
+        int months = 8;
+        double basePrice = 10;
+        double price = 0;
+
+        if (role == "Student") {
+            if (months > 6) {
+                price = basePrice * months * 0.8;
+            }
+            else {
+                price = basePrice * months;
+            }
+        }
+        else if (role == "Teacher") {
+            if (months > 6) {
+                price = basePrice * months * 0.7;
+            }
+            else {
+                price = basePrice * months;
+            }
+        }
+        else if (role == "Guest") {
+            price = basePrice * months;
+        }
+        Console.WriteLine("Final price: " + price);
+    }
+}
